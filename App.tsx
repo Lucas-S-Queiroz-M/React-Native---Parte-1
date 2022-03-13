c559ac571d8e15dfc85bf06623109d79c83d17dc
Lucas-S-Queiroz-M <lucassqm@gmail.com>
2022-03-12 23:28:42 -0300

    Versão 0.1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{borderRadius: 30,flex: 1, backgroundColor:'#E54B4B',  width: '100%', height: '100%'}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#E54B4B',  width: '100%', height: '100%'}}>
+      <Text style={{color:'#fff', fontSize: 40}}>Bem vindo, Carlos</Text>
+      </View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#E54B4B',  width: '100%', height: '100%'}}>
+        <Text style={{color:'#fff'}}>Você gastou hoje</Text>
+      
+        <Text style={{color:'#fff', fontSize: 50}}>R$500,00</Text>
+      </View>
+      
+      <View style={{borderRadius: 25,flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'#E54B4B',  width: '100%', height: '100%'}}>
+        <Text style={{color:'#fff'}}>ESCOLHER PERIODO</Text>
+      
+        <Text style={{color:'#fff'}}>Hoje               Essa Semana           Esse Mês</Text>        
+
+      </View>
+      </View>
+      <View style={styles.listItem}>
+              
+          <Button icon={{name:'flight', color:'#fff'}} buttonStyle={{backgroundColor:'#444140', marginHorizontal: 10}}/>
+                
+          <Button icon={{name:'home', color: '#fff'}} buttonStyle={{backgroundColor:'#444140', marginHorizontal: 10}}/>
+                
+          <Button icon={{name:'local-dining', color: '#fff'}} buttonStyle={{backgroundColor:'#444140', marginHorizontal: 10}}/>
+        
+          <Button icon={{name:'directions-car', color: '#fff'}} buttonStyle={{backgroundColor:'#444140', marginHorizontal: 10}}/>
+        
+          <Button icon={{name:'build', color: '#fff'}} buttonStyle={{backgroundColor:'#444140', marginHorizontal: 10}}/>
+           
+      </View>
+      
+      <View style={styles.container}>
+        <View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%'}}>
+        <View style={styles.sectionStyle}>
+        <TextInput
+            style={{flex: 1}}
+            placeholder="Pizza (R$30,00)                     20/01/2020"           
+        />
+        </View>        
+        
+        </View>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', height: '100%'}}>
+          
+          <View style={styles.sectionStyle}>
+        <TextInput 
+          style={{flex: 1}} 
+          placeholder="Coca-Cola (R$10,00)             20/01/2020"           
+        /> 
+        </View>
+        </View>
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +67,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#F7EBE8',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
+
+  listItem:{
+    margin: 12,  flexDirection: 'row',backgroundColor:'#F7EBE8', alignItems: 'center', marginHorizontal: 1
+  },  
+  
+  sectionStyle: {
+    flexDirection: 'row',    
+    justifyContent: 'space-around',    
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#F7EBE8',    
+    borderRadius: 10,   
+    width: 300,
+  },
+  
 });
